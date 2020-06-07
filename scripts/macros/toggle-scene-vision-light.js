11cbf4c4e9d3181054f5e883320363a496df94d5
Jagusti <jagusti@mundojumbo.net>
2020-06-07 15:04:05 +0100

    Add script source file for Toggle Scene Vision and Light

---
+++ scripts/macros/toggle-scene-vision-light.js
@@ -0,0 +1,19 @@
+/* Toggles the Token Vision and Global Illumination settings. 
+ * If Token Vision is set, Global Illumination is unset (and vice-versa).
+ * Applies to the scene being viewed, which is not necessarily the active scene.
+ */
+
+let thisScene = game.scenes.viewed
+let uiNotice = game.i18n.format("GMTOOLKIT.Message.UnexpectedNoChange");
+
+if (thisScene.data.tokenVision) {
+        thisScene.update({'tokenVision':false})
+        thisScene.update({'globalLight':true})
+        uiNotice = game.i18n.format("GMTOOLKIT.Scene.GlobalNotToken",{sceneName: thisScene.name});
+    } else {
+        thisScene.update({'tokenVision':true})
+        thisScene.update({'globalLight':false})
+        uiNotice = game.i18n.format("GMTOOLKIT.Scene.TokenNotGlobal",{sceneName: thisScene.name});
+    }
+
+ui.notifications.notify(uiNotice);
