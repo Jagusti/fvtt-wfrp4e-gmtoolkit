db7e1958a393f7227305a24b38832d472b76753e
Jagusti <jagusti@mundojumbo.net>
2020-06-09 03:05:30 +0100

    #4 User settings for adding XP

--- scripts/gm-toolkit.js
+++ scripts/gm-toolkit.js
@@ -7,12 +7,29 @@ Hooks.on("init", function() {
 		default: "never",
 		type: String,
 		choices: {
-            "always": "GMTOOLKIT.Settings.always",
-			"never": "GMTOOLKIT.Settings.never",
-			"prompt": "GMTOOLKIT.Settings.prompt",
+            "always": "GMTOOLKIT.Settings.ScenePullActivate.Always",
+			"never": "GMTOOLKIT.Settings.ScenePullActivate.Never",
+			"prompt": "GMTOOLKIT.Settings.ScenePullActivate.Prompt",
 		}
 	});
+	game.settings.register("wfrp4e-gm-toolkit", "addXPDefault", {
+		name: "GMTOOLKIT.Settings.AddXP.Default.name",
+		hint: "GMTOOLKIT.Settings.AddXP.Default.hint",
+		scope: "world",
+		config: true,
+		default: 20,
+		type: Number,
+	});
+	game.settings.register("wfrp4e-gm-toolkit", "addXPPrompt", {
+		name: "GMTOOLKIT.Settings.AddXP.Prompt.name",
+		hint: "GMTOOLKIT.Settings.AddXP.Prompt.hint",
+		scope: "world",
+		config: true,
+		default: false,
+		type: Boolean
+	});
 });
+
 
 Hooks.on('ready', async () => {
 	// Only show warning to GMs
--- scripts/macros/add-xp.js
+++ scripts/macros/add-xp.js
@@ -1,45 +1,84 @@
-/* Adds a set amount of XP to targeted character(s).
- * Adds XP update note to the Chat log.
+/* Adds a set amount of XP to targeted character(s). 
+* Default amount of XP can be preset in module settings, 
+* along with option to prompt for XP amount each time.
+* Non-whole numbers are rounded off. Negative numbers are subtracted. 
+* Adds XP update note to the Chat log.
  */
 
- // Enter Wanted XP.
-let XP = 20; // TODO: Add dialog box for XP. 
-
 if (game.user.targets.size < 1) 
-  return ui.notifications.error(game.i18n.localize("GMTOOLKIT.Token.TargetPCs"));
+return ui.notifications.error(game.i18n.localize("GMTOOLKIT.Token.TargetPCs"));
 
-let targetName = String(); 
-let XPTotal = Number();
-let newXPTotal = Number();
-let XPCurrent = Number();
-let newXPCurrent = Number();
-let chatContent = String("No change made.");
+// Set default XP
+let XP = Number(game.settings.get("wfrp4e-gm-toolkit", "addXPDefault"))
 
-game.user.targets.forEach(target => {
-targetName = target.data.name; 
+// Prompt for XP
+if (game.settings.get("wfrp4e-gm-toolkit", "addXPPrompt")) {
+  const dialog = new Dialog({
+    title: (game.i18n.localize('GMTOOLKIT.Dialog.AddXP.Title')),
+    content: `<form>
+            <div class="form-group">
+            <label>`
+                + game.i18n.localize("GMTOOLKIT.Dialog.AddXP.Prompt") +         
+                `</label> 
+                <input type="text" id="add-xp" name="add-xp" value="`+ XP +`" />
+            </div>
+        </form>`,
+    buttons: {
+      yes: {
+        icon: "<i class='fas fa-check'></i>",
+        label: game.i18n.localize("GMTOOLKIT.Dialog.Apply"),
+        callback: (html) => {
+          let XP = html.find('#add-xp').val();
+          addXP(Number(XP));
+        }
+      },
+      no: {
+        icon: "<i class='fas fa-times'></i>",
+        label: game.i18n.localize("GMTOOLKIT.Dialog.Cancel"),
+      },
+    },
+    default: "yes"
+}).render(true);
+} else {
+  addXP(XP)
+}
 
-if (target.actor.data.type == "character")
-  {
-    XPTotal = target.actor.data.data.details.experience.total; 
-    newXPTotal = XPTotal + XP;
-    XPCurrent = target.actor.data.data.details.experience.current; 
-    newXPCurrent = XPCurrent + XP;
+function addXP(XP) {
+  XP = Math.round(XP);
+  if (isNaN(XP)) return ui.notifications.error(game.i18n.localize('GMTOOLKIT.Dialog.AddXP.InvalidXP'))
 
-    target.actor.update({
-      "data.details.experience.total": newXPTotal
-    })
-        
-    chatContent = game.i18n.format("GMTOOLKIT.AddXP.Success", {XP, targetName, XPTotal, newXPTotal, XPCurrent, newXPCurrent} )
-  } else {
-    chatContent = game.i18n.format("GMTOOLKIT.AddXP.NotPC", {targetName} )
-  }
+  let targetName = String(); 
+  let XPTotal = Number();
+  let newXPTotal = Number();
+  let XPCurrent = Number();
+  let newXPCurrent = Number();
+  let chatContent = Strong(game.i18n.localize("GMTOOLKIT.Dialog.Cancel"));
 
-  let chatData = {
-    user: game.user._id,
-    content: chatContent
-  };
+  game.user.targets.forEach(target => {
+  targetName = target.data.name; 
 
-ChatMessage.create(chatData, {});  
-console.log(chatContent)
+  if (target.actor.data.type == "character")
+    {
+      XPTotal = target.actor.data.data.details.experience.total; 
+      newXPTotal = XPTotal + XP;
+      XPCurrent = target.actor.data.data.details.experience.current; 
+      newXPCurrent = XPCurrent + XP;
 
-})
+      target.actor.update({
+        "data.details.experience.total": newXPTotal
+      })
+          
+      chatContent = game.i18n.format('GMTOOLKIT.AddXP.Success', {XP, targetName, XPTotal, newXPTotal, XPCurrent, newXPCurrent} )
+    } else {
+      chatContent = game.i18n.format('GMTOOLKIT.AddXP.NotPC', {targetName} )
+    }
+
+    let chatData = {
+      user: game.user._id,
+      content: chatContent
+    };
+
+  ChatMessage.create(chatData, {});  
+  console.log(chatContent)
+  })
+}
