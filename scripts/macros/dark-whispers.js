78e90138e362b24d36ad5a6ceb3b5f98c8701903
Jagusti <jagusti@mundojumbo.net>
2021-01-24 04:12:55 +0000

    Restrict who can send and be sent a Dark Whisper
    Adds an explicit restriction to ensure that only GM users can execute the macro. If the current user is not a GM, an error is shown in the user interface. Returning the error from within a macro needs the code to be restructured into a discrete function.
    
    The target selection list is filtered to exclude the current user and any logged in user who does not have an assigned character.
    
    Additionally, this commit
    - adds original macro attribution to Vindico#9013
    - adds semantic emphasis to message wrapper
    - adjusts formatting in chat card, including blockquoting whisper to make it easier to distinguish from surrounding advisory text
    - removes token and 'checked' references

--- scripts/macros/dark-whispers.js
+++ scripts/macros/dark-whispers.js
@@ -1,38 +1,45 @@
-let checkOptions = ""
-let users = game.users.filter(user => user.active);
-let playerTokenIds = users.map(u => u.character?.id).filter(id => id !== undefined);
+/* Open a dialog to send a Dark Whisper (WFRP p183) to selected player character(s).
+ * Original macro developed by Vindico#9103. 
+ */
 
-let selectedPlayerIds = canvas.tokens.controlled.map(token => {
-  if (playerTokenIds.includes(token.actor.id)) return token.actor.id;
-});
+formDarkWhispers();
 
-users.forEach(user => {
-  let checked = !!user.character && selectedPlayerIds.includes(user.character.id) && 'checked';
-  checkOptions+=`
-    <br>
-    <input type="checkbox" name="${user.id}" value="${user.name}" ${checked}>\n
-    <label for="${user.id}">${user.name}</label>
-  `
-});
+function formDarkWhispers() {
 
-new Dialog({
-  title:"Whisper",
-  content:`Whisper To: ${checkOptions} <br>
-    <label for="message">Message:</label>
-    <textarea id="message" name="message" rows="4" cols="50"></textarea><br>`,
-  buttons:{
-    whisper:{   
-      label:"Whisper",
-      callback: (html) => createMessage(html)
-    }
+  // Non-GMs are not permitted to send Dark Whispers
+  if (!game.user.isGM) {      
+    return ui.notifications.error('Only GMs can send Dark Whispers.', {});
   }
-}).render(true);
 
-let messageOpenerRaw = `<strong>Dark Whispers Linger In Your Ears... </strong><br><i>Do not in any way reveal that you have received this message</i><br><br>`
+  // Only other users who are currently logged in and have an assigned character are available to whisper to. 
+  let users = game.users.filter(user => user.active && !user.isSelf && !!user.character);
+  let checkOptions = "";
 
-let messageCloserRaw = `<br><br><i>If you do as directed, your corruption total will be reduced by </i><strong>1</strong>.`
- 
-function createMessage(html) {
+  // Build list of player / characters to select via dialog
+  users.forEach(user => {
+    checkOptions+=`
+        <input type="checkbox" name="${user.id}" value="${user.name}" >\n
+        <label for="${user.id}"> ${user.character.name} (${user.name})</label>
+        <br>
+      `
+  });
+
+  // Show dialog to write message and select target player characters
+  new Dialog({
+    title:"Send Dark Whispers",
+    content:`Whisper To: <br>${checkOptions} <br>
+      <label for="message">Message</label>
+      <textarea id="message" name="message" rows="4" cols="50"></textarea><br>`,
+    buttons:{
+      whisper:{   
+        label:"Whisper",
+        callback: (html) => sendDarkWhispers(html, users)
+      }
+    }
+  }).render(true);
+}
+
+function sendDarkWhispers(html, users) {
   var targets = [];
   // build list of selected players ids for whispers target
   for ( let user of users ) {
@@ -42,8 +49,12 @@ function createMessage(html) {
     var messageText = html.find('[name="message"]')[0].value
   }
 
+  let messageOpenerRaw = `<p><strong>Dark Whispers Linger In Your Ears ... </strong></p><em>Do not in any way reveal that you have received this message.</em>`
+  let messageCloserRaw = `<em>If you do as directed, your Corruption total will be reduced by <strong>1</strong>.<em>`
+
+  // Construct and send message to whisper targets
   ChatMessage.create({
-    content: messageOpenerRaw  + messageText + messageCloserRaw,
-    whisper: targets
-  });
+      content: `${messageOpenerRaw } <blockquote>${messageText}</blockquote> ${messageCloserRaw}`,
+      whisper: targets
+    });
 }
