8339c8c8ff9127887efa8d23946ebdcf6864cc54
Jagusti <jagusti@mundojumbo.net>
2021-01-29 11:31:24 +0000

    Add Send Dark Whispers to macro compendium
    New icon added, and the macro has been added to the GM Toolkit compendium for easy access to importing into the world.
    
    The code has been tidied up to make a bit more readable, including
    - break out dialogContent for constructing the Send Dark Whispers form before inserting into a new dialog process
    - additional in-line explanatory comments

--- scripts/macros/dark-whispers.js
+++ scripts/macros/dark-whispers.js
@@ -39,7 +39,7 @@ function formDarkWhispers(targets=String(game.settings.get("wfrp4e-gm-toolkit", 
       max: game.actors.get(user.character.id).data.data.status.corruption.max
     };
     corruptionAvailable += actorCorruption.value;
-    // Make unselectable if no Corruption to deal with
+    // Make unselectable if character has no Corruption to deal with
     let canWhisperTo = 'disabled title="No Corruption to spend on offer"';
     if (actorCorruption.value >0 ) {canWhisperTo = "enabled"};
 
@@ -52,16 +52,27 @@ function formDarkWhispers(targets=String(game.settings.get("wfrp4e-gm-toolkit", 
       `
   });
   
+  // abort if no characters have any Corruption
   if (corruptionAvailable == 0) {   
     return ui.notifications.error(game.i18n.localize('GMTOOLKIT.Message.DarkWhispers.NoEligibleCharacters'));
   };
 
- // Show dialog to write whisper message and select target player characters
+ // Construct and show form to write whisper message and select target player characters
+ let dialogContent = `
+    <div class="form-group ">
+      <label for="targets">${game.i18n.localize('GMTOOLKIT.Dialog.DarkWhispers.WhisperTargets')} </label>
+    </div>
+    ${checkOptions} // filtered character list
+    <div class="form-group message">
+      <label for="message">${game.i18n.localize('GMTOOLKIT.Dialog.DarkWhispers.WhisperMessage')}</label>
+    </div>
+    <div class="form-group">
+      <textarea id="message" name="message" rows="4" cols="50">${game.wfrp4e.tables.rollTable('darkwhispers').description}</textarea>
+    </div>
+    `
  new Dialog({
   title: game.i18n.localize('GMTOOLKIT.Dialog.DarkWhispers.Title'),
-  content:`<div class="form-group "><label for="targets">${game.i18n.localize('GMTOOLKIT.Dialog.DarkWhispers.WhisperTargets')} </label></div>${checkOptions} 
-    <div class="form-group message"><label for="message">${game.i18n.localize('GMTOOLKIT.Dialog.DarkWhispers.WhisperMessage')}</label></div>
-    <div class="form-group"><textarea id="message" name="message" rows="4" cols="50">${game.wfrp4e.tables.rollTable('darkwhispers').description}</textarea></div>`,
+  content:dialogContent,
   buttons:{
     whisper:{   
       label: game.i18n.localize('GMTOOLKIT.Dialog.DarkWhispers.SendWhisper'),
@@ -72,7 +83,7 @@ function formDarkWhispers(targets=String(game.settings.get("wfrp4e-gm-toolkit", 
 }
 
 function sendDarkWhispers(html, users, sendmode) {
-  // check and prepare message <- TODO move this to the dialog close?
+  // check for whisper message 
   var messageText = html.find('[name="message"]')[0].value;
   
   // build list of selected players ids for whispers target
@@ -95,6 +106,7 @@ function sendDarkWhispers(html, users, sendmode) {
   let whisperMessage = `${game.i18n.format(messageTemplate, {message: messageText})}`
   
   // Add response buttons for chat card
+  // data- attributes are used by listener
   let responseObjects = `
     <span class="chat-card-button-area">
     <a class='chat-card-button darkwhisper-button' data-button='actOnWhisper' data-ask="${game.i18n.format(messageText)}">${game.i18n.localize('GMTOOLKIT.Message.DarkWhispers.Accept')}</a>
