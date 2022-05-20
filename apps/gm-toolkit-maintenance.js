05f50fbdb411b1f19403576e55edf21181ee94f7
Jagusti <jagusti@mundojumbo.net>
2022-05-20 20:39:01 +0100

    Add localization and version metadata to maintenance routine

--- apps/gm-toolkit-maintenance.js
+++ apps/gm-toolkit-maintenance.js
@@ -4,15 +4,15 @@ import { refreshToolkitContent } from "../modules/utility.mjs";
 
 export default class GMToolkitMaintenanceWrapper extends FormApplication {
     async render() {
-        let listToolkitContent = [] 
-        listToolkitContent.macros = (game.macros.filter(m=>m.folder?.name==game.gmtoolkit.module.MODULE_NAME)).sort((a, b) => a.name.localeCompare(b.name))
-        listToolkitContent.tables = (game.tables.filter(m=>m.folder?.name==game.gmtoolkit.module.MODULE_NAME)).sort((a, b) => a.name.localeCompare(b.name))
+        let listToolkitContent = new Array()
+        listToolkitContent.macros = await buildLocalizedContent(game.macros)
+        listToolkitContent.tables = await buildLocalizedContent(game.tables)
         
         let html = await renderTemplate("modules/wfrp4e-gm-toolkit/templates/gm-toolkit-maintenance.html", listToolkitContent)
 
         new GMToolkitMaintenance("wfrp4e-gm-toolkit", `${GMToolkit.MODULE_NAME_FULL} Maintenance`,html).render(true);
     }
-}
+} // end class GMToolkitMaintenanceWrapper
 
 class GMToolkitMaintenance extends Dialog {
 
@@ -38,4 +38,39 @@ class GMToolkitMaintenance extends Dialog {
         })
     }
 
-} 
+} // end class GMToolkitMainenance
+
+async function buildLocalizedContent(documentType) {
+    GMToolkit.log(false, "Starting buildLocalizedContent")
+    let toolkitContent = (documentType.filter(m=>m.folder?.name==game.gmtoolkit.module.MODULE_NAME)).sort((a, b) => a.name.localeCompare(b.name))       
+    let contentArray = [] 
+    let pack = []
+
+    // Set translationKey prefix
+    let translationKeyPrefix = ""
+    if (documentType === game.macros) {
+        translationKeyPrefix = "GMTOOLKIT.Macro"
+        pack = game.packs.get(`${game.gmtoolkit.module.MODULE_ID}.gm-toolkit-macros`);
+    }
+    if (documentType === game.tables) {
+        translationKeyPrefix = "GMTOOLKIT.Table"
+        pack = game.packs.get(`${game.gmtoolkit.module.MODULE_ID}.gm-toolkit-tables`);
+    }
+    
+    // Get Compendium documents
+    let documents = await pack.getDocuments();
+
+    // build localized array
+    for (var content of toolkitContent) {
+        content.translationKey = game.gmtoolkit.utility.strip(content.name, translationKeyPrefix, ".")
+        content.compendiumVersion = documents.filter(d => d.id == content.id).map(i => i.data.flags["wfrp4e-gm-toolkit"].version)
+
+        contentArray.push(content)    
+    }
+    
+    GMToolkit.log(false, "contentArray : ",  contentArray)
+    GMToolkit.log(false, "Ending buildLocalizedContent")
+    
+    return(contentArray)
+    
+}  // end function buildLocalizedContent()
