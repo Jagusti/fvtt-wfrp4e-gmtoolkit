5922135ba01af7582b289120bd9fa757f135da38
Jagusti <jagusti@mundojumbo.net>
2022-05-21 11:57:21 +0100

    Update Advantage macros to reflect refactored Advantage class

--- scripts/macros/reduce-advantage.js
+++ scripts/macros/reduce-advantage.js
@@ -1,9 +1,9 @@
-game.gmtoolkit.advantage.updateAdvantage(token,`reduce`); 
+game.gmtoolkit.advantage.update(token,`reduce`); 
 
 /* ==========
 * MACRO: Reduce Advantage
-* VERSION: 0.8.0
-* UPDATED: 2021-12-26
+* VERSION: 0.9.3
+* UPDATED: 2022-05-21
 * DESCRIPTION: Reduces Advantage for the selected token by 1 (to minimum 0).
 * TIP: Token must be added to an encounter in the Combat Tracker. 
 ========== */
