2d82b49c2100799ec2636e45f7ada3f21de4c732
Jagusti <jagusti@mundojumbo.net>
2022-05-21 09:49:59 +0100

    Update version data for Toggle Scene Vision and Light

--- scripts/macros/toggle-scene-vision-light.js
+++ scripts/macros/toggle-scene-vision-light.js
@@ -1,9 +1,3 @@
-/* Toggles the Token Vision and Unrestricted Vision Range settings. 
- * If Token Vision is set, Unrestricted Vision Range is unset (and vice-versa).
- * Applies to the scene being viewed, which is not necessarily the active scene.
- */
-
-
 let thisScene = game.scenes.viewed;
 let uiNotice = game.i18n.format("GMTOOLKIT.Message.UnexpectedNoChange", {});
 
@@ -16,3 +10,12 @@ if (thisScene.data.tokenVision) {
 }
 
 ui.notifications.notify(uiNotice);
+
+/* ==========
+* MACRO: Toggle Scene Vision and Light
+* VERSION: 0.9.3
+* UPDATED: 2022-01-29
+* DESCRIPTION: Toggles the Token Vision and Unrestricted Vision Range settings. 
+* TIP: If Token Vision is set, Unrestricted Vision Range is unset (and vice-versa).
+* TIP: Applies to the scene being viewed, which is not necessarily the active scene.
+========== */
