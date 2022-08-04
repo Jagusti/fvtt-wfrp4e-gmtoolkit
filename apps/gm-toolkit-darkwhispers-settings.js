c7eb63ee45ece2777c9fa32c39d55086c791dc45
Jagusti <521096+Jagusti@users.noreply.github.com>
2022-08-04 15:26:12 +0100

    Apply linting to settings and group test apps

--- apps/gm-toolkit-darkwhispers-settings.js
+++ apps/gm-toolkit-darkwhispers-settings.js
@@ -1,26 +1,26 @@
-import GMToolkit from "../modules/gm-toolkit.mjs";
-import { getDataSettings } from "../modules/gm-toolkit-settings.mjs";
+import GMToolkit from "../modules/gm-toolkit.mjs"
+import { getDataSettings } from "../modules/gm-toolkit-settings.mjs"
 
 export default class GMToolkitDarkWhispersSettings extends FormApplication {
-    static get defaultOptions() {
-        const options = super.defaultOptions;
-        options.id = "homebrew-settings";
-        options.template = "modules/wfrp4e-gm-toolkit/templates/gm-toolkit-settings.html";
-        options.width = 560;
-        options.minimizable = true;
-        options.resizable = true;
-        options.title = "GMTOOLKIT.Settings.DarkWhispers.menu.title"
-        return options;
-    }
+  static get defaultOptions () {
+    const options = super.defaultOptions
+    options.id = "homebrew-settings"
+    options.template = "modules/wfrp4e-gm-toolkit/templates/gm-toolkit-settings.html"
+    options.width = 560
+    options.minimizable = true
+    options.resizable = true
+    options.title = "GMTOOLKIT.Settings.DarkWhispers.menu.title"
+    return options
+  }
 
-    getData() {
-        let data = super.getData()
-        getDataSettings(data, "darkwhispers");
-        return data
-    }
+  getData () {
+    let data = super.getData()
+    getDataSettings(data, "darkwhispers")
+    return data
+  }
 
-    async _updateObject(event, formData) {
-        for(let setting in formData) 
-            game.settings.set(GMToolkit.MODULE_ID, setting, formData[setting])
-    };
-} 
+  async _updateObject (event, formData) {
+    for (let setting in formData) game.settings
+      .set(GMToolkit.MODULE_ID, setting, formData[setting])
+  }
+}
