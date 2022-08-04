c97b9bb393cca5fbc45e1f24eeaf66465a18ed35
Jagusti <521096+Jagusti@users.noreply.github.com>
2022-08-04 16:16:28 +0100

    Apply linting rules to macro scripts

--- scripts/macros/check-conditions.js
+++ scripts/macros/check-conditions.js
@@ -5,120 +5,131 @@ async function checkConditions () {
 
   if (endOfCombatRoundOnly && !isEndOfRound()) return ui.notifications.error(game.i18n.localize("GMTOOLKIT.Message.CheckConditions.WaitForEndOfRound"))
 
-  const tokens = game.gmtoolkit.utility.getGroup("tokens").filter(g => g.actor.conditions.length !== 0); console.log(tokens)
-  const party = game.gmtoolkit.utility.getGroup("party").filter(g => g.conditions.length !== 0); console.log(party)
+  const tokens = game.gmtoolkit.utility.getGroup("tokens").filter(g => g.actor.conditions.length !== 0)
+  const party = game.gmtoolkit.utility.getGroup("party").filter(g => g.conditions.length !== 0)
 
   const testOptions = {
-    absolute : {difficulty: "challenging"}, 
-    rollMode : "gmroll"
+    absolute: { difficulty: "challenging" },
+    rollMode: "gmroll"
   }
 
   const removedConditions = []
-  
+
   for await (const tokenActor of tokens) {
-    if (skipPCs && party.includes(tokenActor.actor)) continue 
-    testOptions.appendTitle = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.For",  {name: tokenActor.actor.name})
+    if (skipPCs && party.includes(tokenActor.actor)) continue
+    testOptions.appendTitle = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.For", { name: tokenActor.actor.name })
 
     for await (cond of tokenActor.actor.conditions) {
-      await processConditionTest(tokenActor, testOptions, cond.conditionId, removedConditions)
+      await processConditionTest(
+        tokenActor, testOptions, cond.conditionId, removedConditions
+      )
     }
   }
 
   if (removedConditions.length !== 0) ChatMessage.create({ content: removedConditions.join("<br>") })
 
-  return // END OF FUNCTION
+  return removedConditions  // END OF FUNCTION
 
-  function isEndOfRound() {
+  function isEndOfRound () {
     const combat = game.combat
     if (!combat) return false
-    if (combat.data.round != 0 && combat.turns && combat.data.active) {
-      return (combat.current.turn > -1 && combat.current.turn == combat.turns.length - 1)
+    if (combat.data.round !== 0 && combat.turns && combat.data.active) {
+      return (
+        combat.current.turn > -1
+        && combat.current.turn === combat.turns.length - 1
+      )
     }
   }
 
-  async function processConditionTest(tokenActor, testOptions, condition, removedConditions) {
-    if (tokenActor.actor.type === "vehicle" && condition !== "ablaze") return;  // vehicles can burn, but not make condition tests
-    const title = `${game.i18n.localize(game.wfrp4e.config.conditions[(condition)])}`
-    const conditionCount = tokenActor.actor.hasCondition(condition).conditionValue;
+  async function processConditionTest (
+    tokenActor, testOptions, condition, removedConditions
+  ) {
+    if (tokenActor.actor.type === "vehicle" && condition !== "ablaze") return  // Vehicles can burn, but not make condition tests
+    const title = `${game.i18n.localize(game.wfrp4e.config.conditions[condition])}`
+    const conditionCount = tokenActor.actor
+      .hasCondition(condition)
+      .conditionValue
     let conditionRemoved = ""
-    let setupData = {};
-    let conditionTest = {};
-    let skill = {};
-    // show the roll dialog for broken condition tests, so the difficulty can be correctly set
+    let setupData = {}
+    let conditionTest = {}
+    let skill = {}
+    // Show the roll dialog for broken condition tests, so the difficulty can be correctly set
     // note: cancelling the roll dialog cancels any outstanding condition checks
     condition === "broken" ? testOptions.bypass = false : testOptions.bypass = true
 
     switch (condition) {
-      case "surprised": // lose condition
-        tokenActor.actor.removeCondition(condition);
+      case "surprised": // Lose condition
+        tokenActor.actor.removeCondition(condition)
         conditionRemoved = condition
-        break;
-      case "ablaze":  // lose 1d10-TB-AP+(condition-1) (min 1) Wounds
+        break
+      case "ablaze":  // Lose 1d10-TB-AP+(condition-1) (min 1) Wounds
         // doesn't consider the least protected hit location, but does show how much damage is absorbed by AP for manual adjustment if desired
-        const ablazeDamage = await new Roll(`1d10 + ${conditionCount} - 1`).evaluate();
-        console.log(ablazeDamage)
-        tokenActor.actor.applyBasicDamage(ablazeDamage.total, {});
-        break;
-      case "stunned": // challenging Endurance: remove 1+SL (min 1) condition
-        // fall through: 
-      case "poisoned": // challenging Endurance: remove 1+SL (min 1) condition 
-        skill = game.gmtoolkit.utility.hasSkill(tokenActor.actor, game.i18n.localize("NAME.Endurance"), "silent");
-        // setup test data
-        if (skill !== undefined) {   // prefer Endurance test
-          testOptions.title = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.Skill", {title: title, skill: game.i18n.localize("NAME.Endurance")});
+        const ablazeDamage = await new Roll(`1d10 + ${conditionCount} - 1`).evaluate()
+        tokenActor.actor.applyBasicDamage(ablazeDamage.total, {})
+        break
+      case "stunned": // Challenging Endurance: remove 1+SL (min 1) condition
+        // fall through:
+      case "poisoned": // Challenging Endurance: remove 1+SL (min 1) condition
+        skill = game.gmtoolkit.utility.hasSkill(tokenActor.actor, game.i18n.localize("NAME.Endurance"), "silent")
+        // Setup test data
+        if (skill !== undefined) {   // Prefer Endurance test
+          testOptions.title = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.Skill", { title, skill: game.i18n.localize("NAME.Endurance") })
           setupData = await tokenActor.actor.setupSkill(skill, testOptions)
-        } 
-        if (skill === undefined) {  // fallback to Toughness if no Endurance skill
-          testOptions.title = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.Fallback", {title: title, char: game.i18n.localize("CHAR.T")});
-          setupData = await tokenActor.actor.setupCharacteristic("t", testOptions) 
         }
-        // process test
-        await processConditionTest();
-        break;
-      case "broken":  // challenging Cool: remove 1+SL (min 1) condition
-        skill = game.gmtoolkit.utility.hasSkill(tokenActor.actor, game.i18n.localize("NAME.Cool"), "silent");
-        // setup test data
-        if (skill !== undefined) {   // prefer Cool test
-          testOptions.title = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.Skill", {title: title, skill: game.i18n.localize("NAME.Cool")});
+        if (skill === undefined) {  // Fallback to Toughness if no Endurance skill
+          testOptions.title = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.Fallback", { title, char: game.i18n.localize("CHAR.T") })
+          setupData = await tokenActor.actor.setupCharacteristic("t", testOptions)
+        }
+        // Process test
+        await processConditionTest()
+        break
+      case "broken":  // Challenging Cool: remove 1+SL (min 1) condition
+        skill = game.gmtoolkit.utility.hasSkill(tokenActor.actor, game.i18n.localize("NAME.Cool"), "silent")
+        // Setup test data
+        if (skill !== undefined) {   // Prefer Cool test
+          testOptions.title = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.Skill", { title, skill: game.i18n.localize("NAME.Cool") })
           setupData = await tokenActor.actor.setupSkill(skill, testOptions)
-        } 
-        if (skill === undefined) {  // fallback to Willpower if no Cool skill
-          testOptions.title = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.Fallback", {title: title, char: game.i18n.localize("CHAR.WP")});
-          setupData = await tokenActor.actor.setupCharacteristic("t", testOptions) 
         }
-        // process test
-        await processConditionTest();
-        break;
+        if (skill === undefined) {  // Fallback to Willpower if no Cool skill
+          testOptions.title = game.i18n.format("GMTOOLKIT.Dialog.CheckConditions.Fallback", { title, char: game.i18n.localize("CHAR.WP") })
+          setupData = await tokenActor.actor.setupCharacteristic("t", testOptions)
+        }
+        // Process test
+        await processConditionTest()
+        break
       default:
-        break;
-    }    
-    // collect removed conditions
+        break
+    }
+    // Collect removed conditions
     if (conditionRemoved !== "") {
       removedConditions.push(
         game.i18n.format("CHAT.RemovedConditions", {
           condition: title,
-          name: tokenActor.actor.name 
+          name: tokenActor.actor.name
         })
       )
     }
 
-    async function processConditionTest() {
-      conditionTest = await tokenActor.actor.basicTest(setupData);
+    async function processConditionTest () {
+      conditionTest = await tokenActor.actor.basicTest(setupData)
       if (conditionTest.succeeded) {
-        tokenActor.actor.removeCondition(condition, Math.min(Number(conditionTest.result.SL) + 1, conditionCount));
-        conditionRemoved = condition;
+        tokenActor.actor.removeCondition(
+          condition,
+          Math.min(Number(conditionTest.result.SL) + 1, conditionCount)
+        )
+        conditionRemoved = condition
       }
       return conditionTest
     }
-  } // end processConditionTest
+  } // End processConditionTest
 }
 
 
 /* ==========
 * MACRO: Check Conditions
-* VERSION: 0.9.4.2
-* UPDATED: 2022-07-25
+* VERSION: 0.9.5
+* UPDATED: 2022-08-04
 * DESCRIPTION: Process end of round condition checks. Automatically handle removal of Surprised condition, tests to remove Poisoned, Stunned and Broken conditions, and Ablaze damage (including to vehicles).
-* TIP: Set `skipPCs = false` to automatically make condition checks for player-assigned characters. 
-* TIP: Set `endOfCombatRoundsOnly = false` to use the macro in any combat round, or even outside combat. 
+* TIP: Set `skipPCs = false` to automatically make condition checks for player-assigned characters.
+* TIP: Set `endOfCombatRoundsOnly = false` to use the macro in any combat round, or even outside combat.
 ========== */
--- scripts/macros/clear-advantage.js
+++ scripts/macros/clear-advantage.js
@@ -1,10 +1,9 @@
-game.gmtoolkit.advantage.update(token,`clear`);
+game.gmtoolkit.advantage.update(token, "clear")
 
 /* ==========
 * MACRO: Clear Advantage
-* VERSION: 0.9.3
-* UPDATED: 2022-05-21
+* VERSION: 0.9.5
+* UPDATED: 2022-08-04
 * DESCRIPTION: Resets Advantage for the selected token to 0.
-* TIP: Token does not have to be added to an encounter in the Combat Tracker. 
+* TIP: Token does not have to be added to an encounter in the Combat Tracker.
 ========== */
-
