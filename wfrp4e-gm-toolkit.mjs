da49dc418a014594c0aef8da711afd333b8c6bc8
Jagusti <521096+Jagusti@users.noreply.github.com>
2022-08-05 02:39:19 +0100

    Apply linting rules to initialisation script

--- wfrp4e-gm-toolkit.mjs
+++ wfrp4e-gm-toolkit.mjs
@@ -5,43 +5,43 @@
  * Issue Tracker: https://github.com/Jagusti/fvtt-wfrp4e-gmtoolkit/issues
  */
 
+// Import Helpers
+import GMToolkit from "./modules/gm-toolkit.mjs"
+import * as GMToolkitUtility from "./modules/utility.mjs"
+
 // Import Modules
-import GMToolkit from "./modules/gm-toolkit.mjs";
-import { GMToolkitSettings, registerGroupTestSettings } from "./modules/gm-toolkit-settings.mjs";
-import Advantage from "./modules/advantage.mjs";
+import { GMToolkitSettings, registerGroupTestSettings } from "./modules/gm-toolkit-settings.mjs"
+import Advantage from "./modules/advantage.mjs"
 import { runSilentGroupTest, runGroupTest } from "./modules/group-test.mjs"
 import { GroupTest } from "./apps/group-test.js"
-import DarkWhispers from "./modules/dark-whispers.mjs";
-import TokenHudExtension from "./modules/token-hud-extension.mjs";
-
-// Import Helpers
-import * as GMToolkitUtility from "./modules/utility.mjs";
+import DarkWhispers from "./modules/dark-whispers.mjs"
+import TokenHudExtension from "./modules/token-hud-extension.mjs"
 
 
 /* -------------------------------------------- */
 /*  Foundry VTT Initialisation                  */
 /* -------------------------------------------- */
 
-/** 
+/**
  * Carry out initialisation routines
  */
 Hooks.once("init", function () {
-    GMToolkit.log(false ,`Initialising ${GMToolkit.MODULE_NAME}.`);
+  GMToolkit.log(false, `Initialising ${GMToolkit.MODULE_NAME}.`)
 
-    // Create namespace within game global
-    game.gmtoolkit = {
-        advantage : Advantage,
-        utility : GMToolkitUtility, 
-        module : GMToolkit,
-        grouptest: {
-          launch: GroupTest,
-          run: runGroupTest,
-          silent: runSilentGroupTest,
-        },
-        skills: []
-    }
+  // Create namespace within game global
+  game.gmtoolkit = {
+    advantage: Advantage,
+    utility: GMToolkitUtility,
+    module: GMToolkit,
+    grouptest: {
+      launch: GroupTest,
+      run: runGroupTest,
+      silent: runSilentGroupTest
+    },
+    skills: []
+  }
 
-});
+})
 
 
 /* -------------------------------------------- */
@@ -58,33 +58,33 @@ Hooks.once("ready", async function () {
   }
 
   // Register module settings
-  await GMToolkitSettings.register();
+  await GMToolkitSettings.register()
 
-  GMToolkit.log(false ,`${GMToolkit.MODULE_NAME} is ready.`);
+  GMToolkit.log(false, `${GMToolkit.MODULE_NAME} is ready.`)
 
   // Notify any player users that do not have characters assigned
-  const spectators = GMToolkitUtility.getGroup("spectators").map(i => (" " + i.name))
+  const spectators = GMToolkitUtility.getGroup("spectators").map(i => ` ${i.name}`)
   if (spectators.length > 0) {
     GMToolkit.log(true, `Spectators: ${spectators}`)
-    if (!game.settings.get("wfrp4e-gm-toolkit","suppressSpectatorNotice")) {
-      ui.notifications.error(`${game.i18n.format("GMTOOLKIT.Message.Spectators", {spectators: spectators})}`, {permanent: true})
-    } 
+    if (!game.settings.get("wfrp4e-gm-toolkit", "suppressSpectatorNotice")) {
+      ui.notifications.error(`${game.i18n.format("GMTOOLKIT.Message.Spectators", { spectators })}`, { permanent: true })
+    }
   }
 
   // Register Handlebar helper to find an entry within an array
-  Handlebars.registerHelper('ifIn', function(toFind, inList) {
-    if(inList.indexOf(toFind) > -1) {
-      return true;
+  Handlebars.registerHelper("ifIn", function (toFind, inList) {
+    if (inList.indexOf(toFind) > -1) {
+      return true
     }
-  });
-  
-});
+  })
+
+})
 
 Hooks.on("ready", async () => {
   game.socket.on(`module.${GMToolkit.MODULE_ID}`, data => {
     SocketHandlers[data.type](data)
   })
-});
+})
 
 /* -------------------------------------------- */
 /*  Other Hooks                                 */
@@ -92,22 +92,22 @@ Hooks.on("ready", async () => {
 
 // Register module debug flag with Developer Mode custom hook
 Hooks.once("devModeReady", ({ registerPackageDebugFlag }) => {
-  registerPackageDebugFlag(GMToolkit.MODULE_ID);
-});
+  registerPackageDebugFlag(GMToolkit.MODULE_ID)
+})
 
 // Disable movement on holding scene
 Hooks.on("preUpdateToken", (token, change) => {
-	GMToolkit.log(false, `${token.data.x} -> ${change?.x}, ${token.data.y} -> ${change?.y}`)
-	if (!(game.user.isGM) && game.canvas.scene.name == game.settings.get("wfrp4e-gm-toolkit", "holdingScene")) { 
-		if (change?.x) {change.x = token.data.x}
-		if (change?.y) {change.y = token.data.y}
-	}
-}); 
+  GMToolkit.log(false, `${token.data.x} -> ${change?.x}, ${token.data.y} -> ${change?.y}`)
+  if (!game.user.isGM && game.canvas.scene.name === game.settings.get("wfrp4e-gm-toolkit", "holdingScene")) {
+    if (change?.x) {change.x = token.data.x}
+    if (change?.y) {change.y = token.data.y}
+  }
+})
 
 // Display Token Hud Extension if enabled
-Hooks.on("renderTokenHUD", (app, html, data) => { 
-  if (game.settings.get(GMToolkit.MODULE_ID, "enableTokenHudExtensions")) TokenHudExtension.addTokenHudExtensions(app, html, data) 
-});
+Hooks.on("renderTokenHUD", (app, html, data) => {
+  if (game.settings.get(GMToolkit.MODULE_ID, "enableTokenHudExtensions")) TokenHudExtension.addTokenHudExtensions(app, html, data)
+})
 
 // If Babele is installed, wait until it completed initialisation and then compile localized skills list used for Group Tests
 Hooks.once("babele.ready", async function () {
@@ -115,41 +115,45 @@ Hooks.once("babele.ready", async function () {
   if (!game.babele || game.babele.initialized) {
     console.log("Building skills now Babele is ready")
     game.gmtoolkit.skills = await GMToolkitUtility.compileItems(["skill"])
-    registerGroupTestSettings() 
+    registerGroupTestSettings()
   }
-});
-  
+})
+
 /* -------------------------------------------- */
 /*  Socket Handlers                                 */
 /* -------------------------------------------- */
 
 export class SocketHandlers {
 
-    // Used for updating Advantage when the opposed test is resolved by a player character that does not own the opposing character
-    static async updateAdvantage(data) {
-        console.log("Socket: updateAdvantage", data)
-        if (!game.user.isUniqueGM) return
-        let updated = ""
-        let character = (data.payload.character)
-        return updated = await game.scenes.active.tokens.filter(t => t.actor.id == character)[0].actor.update(data.payload.updateData);
-      }
-      
-      // Used for updating Advantage flags when the opposed test is resolved by a player character that does not own the opposing character
-      static async setFlag(data) {
-        console.log("Socket: setFlag", data)
-        if (!game.user.isUniqueGM) return // ui.notifications.notify("Not setting flag on behalf of player", "warn", {permanent: true})
-        ui.notifications.notify(`Setting flag "${data.payload.updateData.key}" on ${data.payload.character.name} as GM`, "info", {permanent: true})
-        
-        let character = (data.payload.character)
-        // console.log("data.payload.character", character)
-        
-        let actorToChange = game.scenes.active.tokens.filter(t => t.actor.id == character.actorId)[0].actor
-        // console.log("actorToChange", actorToChange)
-        
-        let updated = ""
-        return updated = await actorToChange.setFlag(GMToolkit.MODULE_ID, data.payload.updateData.flag, {[data.payload.updateData.key]: data.payload.updateData.value});
-        // console.log(actorToChange.document.getFlag(GMToolkit.MODULE_ID, `${data.payload.updateData.flag}`));
+  // Used for updating Advantage when the opposed test is resolved by a player character that does not own the opposing character
+  static async updateAdvantage (data) {
+    console.log("Socket: updateAdvantage", data)
+    if (!game.user.isUniqueGM) return
+    let updated = ""
+    const { character } = data.payload
+    return updated = await game.scenes.active.tokens
+      .filter(t => t.actor.id === character)[0].actor
+      .update(data.payload.updateData)
+  }
 
-    }
+  // Used for updating Advantage flags when the opposed test is resolved by a player character that does not own the opposing character
+  static async setFlag (data) {
+    console.log("Socket: setFlag", data)
+    if (!game.user.isUniqueGM) return
+    ui.notifications.notify(`Setting flag "${data.payload.updateData.key}" on ${data.payload.character.name} as GM`, "info", { permanent: true })
+
+    const { character } = data.payload
+
+    const actorToChange = game.scenes.active.tokens
+      .filter(t => t.actor.id === character.actorId)[0].actor
+
+    let updated = ""
+    return updated = await actorToChange.setFlag(
+      GMToolkit.MODULE_ID,
+      data.payload.updateData.flag,
+      { [data.payload.updateData.key]: data.payload.updateData.value }
+    )
+
+  }
 
 }
