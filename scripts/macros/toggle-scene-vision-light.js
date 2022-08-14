383ccc5a49fdebc90a626fdaaa528f856348d29e
Jagusti <521096+Jagusti@users.noreply.github.com>
2022-08-14 21:32:52 +0100

    Make Toggle Scene Vision and Light v10 compatible
    - reference revised datamodel for scene
    - revert Unrestricted Vision Range to Global Illumination (FVTT 10.274)
    - prevent duplicate console logging #161

--- scripts/macros/toggle-scene-vision-light.js
+++ scripts/macros/toggle-scene-vision-light.js
@@ -1,21 +1,20 @@
 let thisScene = game.scenes.viewed
 let uiNotice = game.i18n.format("GMTOOLKIT.Message.UnexpectedNoChange", {})
 
-if (thisScene.data.tokenVision) {
-  thisScene.update({ tokenVision: false, globalLight: true })
-  uiNotice = game.i18n.format("GMTOOLKIT.Scene.UnrestrictedNotToken", { sceneName: thisScene.name }, {})
-} else {
-  thisScene.update({ tokenVision: true, globalLight: false })
-  uiNotice = game.i18n.format("GMTOOLKIT.Scene.TokenNotUnrestricted", { sceneName: thisScene.name }, {})
-}
+await thisScene.update( { tokenVision: !thisScene.tokenVision})
+await thisScene.update( { globalLight: !thisScene.tokenVision})
 
-ui.notifications.notify(uiNotice)
+uiNotice = (thisScene.tokenVision) 
+  ? game.i18n.format("GMTOOLKIT.Scene.TokenNotGlobal", { sceneName: thisScene.name })
+  : game.i18n.format("GMTOOLKIT.Scene.GlobalNotToken", { sceneName: thisScene.name })
+
+ui.notifications.info(uiNotice, { console: false })
 
 /* ==========
 * MACRO: Toggle Scene Vision and Light
 * VERSION: 0.9.5
-* UPDATED: 2022-08-04
-* DESCRIPTION: Toggles the Token Vision and Unrestricted Vision Range settings.
-* TIP: If Token Vision is set, Unrestricted Vision Range is unset (and vice-versa).
+* UPDATED: 2022-08-14
+* DESCRIPTION: Toggles the Token Vision and Global Illumination settings.
+* TIP: If Token Vision is set, Global Illumination is unset (and vice-versa).
 * TIP: Applies to the scene being viewed, which is not necessarily the active scene.
 ========== */
