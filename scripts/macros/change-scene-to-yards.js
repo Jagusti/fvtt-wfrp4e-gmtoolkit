84c326dd4aec9055d35e1782f25ad6b20b55ff5c
Tancrede Chazallet <tancrede@bitbuildr.com>
2022-09-29 23:11:12 +0100

    Change scene to yards

---
+++ scripts/macros/change-scene-to-yards.js
@@ -0,0 +1,20 @@
+const scale = 2 / canvas.scene.data.gridDistance;
+await canvas.scene.update({
+"gridDistance": 2,
+"gridUnits": "yd"
+});
+const newLights = canvas.scene.data.lights.map(light => {
+  return {
+    _id: light.id,
+    "config.dim": light.data.config.dim * scale,
+    "config.bright": light.data.config.bright * scale
+  };
+});
+await canvas.scene.updateEmbeddedDocuments("AmbientLight", newLights);
+
+/* ==========
+* MACRO: Change scene to yards
+* VERSION: 1.0
+* UPDATED: 2022-09-29
+* DESCRIPTION: Change scene grid to 2 yards, keeping lighting at scale. Useful for maps created for DnD that have a grid of 5ft, or any other grid size.
+========== */
