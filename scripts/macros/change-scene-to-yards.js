695d1e691513703b275aa9fa57a92b6e28cab1e9
Tancrède Chazallet <ancainu@gmail.com>
2023-04-25 01:32:37 +0100

    New macro: Change scene to yards (#189)
    * Change scene to yards
    
    * Compatible with Foundry v10
    
    * Add macro icon
    
    * Format macro
    
    * Add Change scene to yards to macro compendium
    
    * Add localisation
    
    * Add to Macro Toolbox
    
    * Add details to Changelog
    
    ---------
    
    Co-authored-by: Jagusti <521096+Jagusti@users.noreply.github.com>

---
+++ scripts/macros/change-scene-to-yards.js
@@ -0,0 +1,18 @@
+const scale = 2 / canvas.scene.grid.distance
+await canvas.scene.update({
+  "grid.distance": 2,
+  "grid.units": "yd"
+})
+const newLights = canvas.scene.lights.map(light => ({
+  _id: light.id,
+  "config.dim": light.config.dim * scale,
+  "config.bright": light.config.bright * scale
+}))
+await canvas.scene.updateEmbeddedDocuments("AmbientLight", newLights)
+
+/* ==========
+* MACRO: Change scene to yards
+* VERSION: 1.0
+* UPDATED: 2023-03-08
+* DESCRIPTION: Change scene grid to 2 yards, keeping lighting at scale. Useful for maps created for DnD that have a grid of 5ft, or any other grid size.
+========== */
