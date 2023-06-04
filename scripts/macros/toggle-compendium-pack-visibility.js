c039b8f860b0f532c700b6b78349c43c90967bda
Jagusti <521096+Jagusti@users.noreply.github.com>
2023-06-04 18:08:39 +0100

    Update Toggle Compendium Pack Visibility to use ownership #225

--- scripts/macros/toggle-compendium-pack-visibility.js
+++ scripts/macros/toggle-compendium-pack-visibility.js
@@ -6,21 +6,21 @@ let packtypes = [] // Leave empty to include all types of compendium pack
 
 let packs = []
 
-// eslint-disable-next-line capitalized-comments
-let forcePrivate = false // false: toggle current visibility setting for pack; true: force to not visible
+let forceLimited = false // false: toggle current visibility setting for pack; true: force to not visible
 
 if (packsource.length === 0 && packtypes.length === 0 ) packs = game.packs.filter(p => p.metadata.system === "wfrp4e")
 
 if (packsource.length > 0 && packtypes.length === 0 ) {
   packs = game.packs.filter(p => {
     for (const source of packsource) {
-      if (p.metadata.package === source) return true
+      if (p.metadata.packageName === source) return true
     }
     return false
   })
 }
 
 if (packsource.length === 0 && packtypes.length > 0) {
+  console.log(packtypes)
   packs = game.packs.filter(p => p.metadata.system === "wfrp4e").filter(p => {
     for (const type of packtypes) {
       if (p.metadata.type === type) return true
@@ -29,12 +29,12 @@ if (packsource.length === 0 && packtypes.length > 0) {
   })
 }
 
-
 if (packsource.length > 0 && packtypes.length > 0) {
+  console.log(packsource, packtypes)
   packs = game.packs.filter(p => {
-    for (const compendium of packsource) {
+    for (const source of packsource) {
       for (const type of packtypes) {
-        if (p.metadata.package === compendium
+        if (p.metadata.packageName === source
           && p.metadata.type === type) return true
       }
     }
@@ -42,20 +42,62 @@ if (packsource.length > 0 && packtypes.length > 0) {
   })
 }
 
-game.gmtoolkit.module.log(true, packs)
+console.log(packs)
 
-for (const pack of packs) {
-  if (forcePrivate) {
-    await pack.configure({ private: true })
-  } else {
-    await pack.configure({ private: !pack.private })
+// Add support for v10 and v11
+if (game.release.generation < 11) {
+  togglePrivate(packs, forceLimited)
+} else {
+  togglePermissions(packs, forceLimited)
+}
+
+async function togglePermissions (packs, force) {
+  const permsLimited = {
+    ASSISTANT: "OWNER",
+    GAMEMASTER: "OWNER",
+    PLAYER: "LIMITED",
+    TRUSTED: "LIMITED"
+  }
+  const permsObserver = {
+    ASSISTANT: "OWNER",
+    GAMEMASTER: "OWNER",
+    PLAYER: "OBSERVER",
+    TRUSTED: "OBSERVER"
+  }
+
+  for (const pack of packs) {
+    if (force) {
+      await pack.configure({ ownership: permsLimited })
+    } else {
+      switch (pack.ownership.PLAYER) {
+        case "LIMITED":
+          await pack.configure({ ownership: permsObserver })
+          break
+        case "OBSERVER":
+        default:
+          await pack.configure({ ownership: permsLimited })
+      }
+    }
   }
 }
 
+async function togglePrivate (packs, force) {
+  for (const pack of packs) {
+    if (force) {
+      await pack.configure({ private: true })
+    } else {
+      await pack.configure({ private: !pack.private })
+    }
+  }
+}
+
+
 /* ==========
 * MACRO: Toggle Compendium Pack Visibility
-* VERSION: 0.9.5
-* UPDATED: 2022-08-04
-* DESCRIPTION: Hides (or shows) compendium packs designated for the wfrp4e system
+* VERSION: 6.0.4
+* UPDATED: 2023-06-04
+* DESCRIPTION: Toggles effective visibility of compendium packs
 * TIP: If no compendium source is specified, only modules declared for the "wfrp4e" system are included
+* TIP: From v11, toggles between Limited and Observer ownership permissions on compendium packs for players and trusted players
+* TIP: Prior to v11, toggles private flag on packs
 ========== */
