544b6c32afbee8ce5b5d0b8ebdce94c111c99ca4
Jagusti <521096+Jagusti@users.noreply.github.com>
2025-04-19 22:26:47 +0100

    Convert Session Management Settings UI to ApplicationV2 #294

--- apps/gm-toolkit-session-management-settings.js
+++ apps/gm-toolkit-session-management-settings.js
@@ -1,27 +1,70 @@
 import GMToolkit from "../modules/gm-toolkit.mjs"
-import { getDataSettings } from "../modules/gm-toolkit-settings.mjs"
+import { prepareSettingsFormData } from "../modules/gm-toolkit-settings.mjs"
 
 export default class GMToolkitSessionManagementSettings
-  extends FormApplication {
-  static get defaultOptions () {
-    const options = super.defaultOptions
-    options.id = "homebrew-settings"
-    options.template = "modules/wfrp4e-gm-toolkit/templates/gm-toolkit-settings.html"
-    options.width = 560
-    options.minimizable = true
-    options.resizable = true
-    options.title = "GMTOOLKIT.Settings.SessionManagement.menu.title"
-    return options
+  extends HandlebarsApplicationMixin(ApplicationV2) {
+
+  static DEFAULT_OPTIONS = {
+    id: "gmtoolkit-settings-session",
+    tag: "form",
+    form: {
+      handler: GMToolkitSessionManagementSettings.onSubmit,
+      submitOnChange: false,
+      closeOnSubmit: false
+    },
+    actions: {
+      add: GMToolkitSessionManagementSettings.increaseValue,
+      subtract: GMToolkitSessionManagementSettings.decreaseValue
+    },
+    position: { width: 560 },
+    window: {
+      icon: "fas fa-gear",
+      title: "GMTOOLKIT.Settings.SessionManagement.menu.title",
+      contentClasses: ["standard-form"]
+    }
   }
 
-  getData () {
-    const data = super.getData()
-    getDataSettings(data, "session")
-    return data
+  static PARTS = {
+    form: {
+      template: "modules/wfrp4e-gm-toolkit/templates/gm-toolkit-settings-V2.html"
+    },
+    footer: {
+      template: "templates/generic/form-footer.hbs"
+    }
   }
 
-  async _updateObject (event, formData) {
-    for (let setting in formData) game.settings
-      .set(GMToolkit.MODULE_ID, setting, formData[setting])
+  async _prepareContext (options) {
+    const context = await super._prepareContext(options)
+
+    context.settings = await prepareSettingsFormData("session")
+    context.buttons = [
+      {
+        type: "submit",
+        icon: "fa-solid fa-save",
+        label: "Submit",
+        action: "submit"
+      }
+    ]
+
+    return context
+  }
+
+  static increaseValue (event, target) {
+    const input = target.form.querySelector(`input[name=${target.dataset.name}]`)
+    const newVal = input.valueAsNumber + (event.shiftKey ? 10 : 1)
+    input.value = newVal > Number(input.max) ? input.max : newVal
+  }
+
+  static decreaseValue (event, target) {
+    const input = target.form.querySelector(`input[name=${target.dataset.name}]`)
+    const newVal = input.valueAsNumber - (event.shiftKey ? 10 : 1)
+    input.value = newVal < 0 ? 0 : newVal
+  }
+
+  static async onSubmit (event, form, formData) {
+    const inputFields = formData.object
+    for (const setting in inputFields) game.settings
+      .set(GMToolkit.MODULE_ID, setting, inputFields[setting])
+    if (event.type === "submit") this.close(false)
   }
 }
