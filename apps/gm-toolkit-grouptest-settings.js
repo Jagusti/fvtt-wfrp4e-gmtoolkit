3bde11c6f960c235fd8106e14649f17b88261798
Jagusti <521096+Jagusti@users.noreply.github.com>
2025-04-19 23:31:38 +0100

    Convert Group Test Settings UI to ApplicationV2 #292

--- apps/gm-toolkit-grouptest-settings.js
+++ apps/gm-toolkit-grouptest-settings.js
@@ -1,26 +1,56 @@
 import GMToolkit from "../modules/gm-toolkit.mjs"
-import { getDataSettings } from "../modules/gm-toolkit-settings.mjs"
+import { prepareSettingsFormData } from "../modules/gm-toolkit-settings.mjs"
 
-export default class GMToolkitGroupTestSettings extends FormApplication {
-  static get defaultOptions () {
-    const options = super.defaultOptions
-    options.id = "homebrew-settings"
-    options.template = "modules/wfrp4e-gm-toolkit/templates/gm-toolkit-settings.html"
-    options.width = 560
-    options.minimizable = true
-    options.resizable = true
-    options.title = "GMTOOLKIT.Settings.GroupTest.menu.name"
-    return options
+export default class GMToolkitGroupTestSettings
+  extends HandlebarsApplicationMixin(ApplicationV2) {
+
+  static DEFAULT_OPTIONS = {
+    id: "gmtoolkit-settings-grouptest",
+    tag: "form",
+    form: {
+      handler: GMToolkitGroupTestSettings.onSubmit,
+      submitOnChange: false,
+      closeOnSubmit: true
+    },
+    position: {
+      width: 560,
+      height: 600
+    },
+    window: {
+      icon: "fas fa-gear",
+      title: "GMTOOLKIT.Settings.GroupTest.menu.name",
+      contentClasses: ["standard-form", "gmtoolkit scrollable"]
+    }
   }
 
-  getData () {
-    const data = super.getData()
-    getDataSettings(data, "grouptest")
-    return data
+  static PARTS = {
+    form: {
+      template: "modules/wfrp4e-gm-toolkit/templates/gm-toolkit-settings-V2.html"
+    },
+    footer: {
+      template: "templates/generic/form-footer.hbs"
+    }
   }
 
-  async _updateObject (event, formData) {
-    for (let setting in formData) game.settings
-      .set(GMToolkit.MODULE_ID, setting, formData[setting])
+  async _prepareContext (options) {
+    const context = await super._prepareContext(options)
+
+    context.settings = await prepareSettingsFormData("grouptest")
+    context.buttons = [
+      {
+        type: "submit",
+        icon: "fa-solid fa-save",
+        label: "Submit",
+        action: "submit"
+      }
+    ]
+
+    return context
+  }
+
+  static async onSubmit (event, form, formData) {
+    const inputFields = formData.object
+    for (const setting in inputFields) game.settings
+      .set(GMToolkit.MODULE_ID, setting, inputFields[setting])
   }
 }
