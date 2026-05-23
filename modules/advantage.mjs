1ab07520cdbb559bd9f4f5242e6691f559bb1e97
Jagusti <521096+Jagusti@users.noreply.github.com>
2026-05-23 23:23:08 +0100

    Resolve issues with applying and presenting Damage Console results

--- modules/advantage.mjs
+++ modules/advantage.mjs
@@ -297,8 +297,8 @@ export default class Advantage {
 
 Hooks.on("wfrp4e:applyDamage", async function (scriptArgs) {
   GMToolkit.log(false, scriptArgs)
-  if (!scriptArgs.opposedTest.defenderTest.context.unopposed) return // Only apply when Outmanouevring (ie, damage from an unopposed test).
-  if (scriptArgs.opposedTest.attackerTest.preData.dualWielding) return // Exit if this is the first strike when Dual Wielding
+  if (!scriptArgs.opposedTest?.defenderTest?.context?.unopposed) return // Only apply when Outmanouevring (ie, damage from an unopposed test).
+  if (scriptArgs.opposedTest?.attackerTest?.preData?.dualWielding) return // Exit if this is the first strike when Dual Wielding
   if (!game.settings.get(GMToolkit.MODULE_ID, "automateDamageAdvantage")) return
   if (!inActiveCombat(scriptArgs.opposedTest.attackerTest.actor)
     | !inActiveCombat(scriptArgs.opposedTest.defenderTest.actor)) return // Exit if either actor is not in the active combat
