37646cdae4a7b62918c8cd89458953fce4e35ec9
Jagusti <521096+Jagusti@users.noreply.github.com>
2026-05-25 12:21:34 +0100

    Update Toggle Scene Vision and Light macro to use scene environment data model

--- scripts/macros/toggle-scene-vision-light.js
+++ scripts/macros/toggle-scene-vision-light.js
@@ -1,10 +1,11 @@
 let thisScene = game.scenes.viewed
 let uiNotice = game.i18n.format("GMTOOLKIT.Message.UnexpectedNoChange", {})
 
-await thisScene.update( { tokenVision: !thisScene.tokenVision})
-await thisScene.update( { globalLight: !thisScene.tokenVision})
+const visibility = !thisScene.tokenVision
+await thisScene.update( { tokenVision: visibility })
+await thisScene.update( { "environment.globalLight.enabled": !visibility })
 
-uiNotice = (thisScene.tokenVision) 
+uiNotice = (thisScene.tokenVision)
   ? game.i18n.format("GMTOOLKIT.Scene.TokenNotGlobal", { sceneName: thisScene.name })
   : game.i18n.format("GMTOOLKIT.Scene.GlobalNotToken", { sceneName: thisScene.name })
 
@@ -12,8 +13,8 @@ ui.notifications.info(uiNotice, { console: false })
 
 /* ==========
 * MACRO: Toggle Scene Vision and Light
-* VERSION: 0.9.5
-* UPDATED: 2022-08-14
+* VERSION: 9.2.1
+* UPDATED: 2026-05-25
 * DESCRIPTION: Toggles the Token Vision and Global Illumination settings.
 * TIP: If Token Vision is set, Global Illumination is unset (and vice-versa).
 * TIP: Applies to the scene being viewed, which is not necessarily the active scene.
